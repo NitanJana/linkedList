d212ff09698bd3bef5c61b4db56219df7f17307c
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-10-30 09:38:01 +0530

    feat: add append method

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import LinkedList from "./modules/linkedList.js";
 const list = new LinkedList();
 
 console.log(list);
+list.append(15);
+console.log(list);
+list.append(25);
+console.log(list);
--- src/modules/linkedList.js
+++ src/modules/linkedList.js
@@ -1,7 +1,19 @@
-// import Node from "./node.js";
+import Node from "./node.js";
 
 export default class LinkedList {
   constructor() {
-    this.listHead = null;
+    this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) tempNode = tempNode.next;
+      tempNode.next = newNode;
+    }
   }
 }
